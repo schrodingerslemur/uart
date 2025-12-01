eb27f4f6c38f63fccd47bc7d1087cbc95a8479b3
Brendan Hendrata <bhendrata@cmu.edu>
2025-12-01 16:11:02 -0500

    added datapath instantiation

--- uart_rx.sv
+++ uart_rx.sv
@@ -10,7 +10,7 @@ module uart_rx #(
   output logic rx_valid
 );
 
-  localparam int CLKS_PER_SAMPLE = ClK_FREQ / (BAUD_RATE * OVERSAMPLE);
+  localparam int CLKS_PER_SAMPLE = CLK_FREQ / (BAUD_RATE * OVERSAMPLE);
 
   // Parameters ---
   /*
@@ -25,65 +25,16 @@ module uart_rx #(
 
   // Oversampling ---
   logic [$clog2(CLKS_PER_SAMPLE)-1:0] clk_count;
-  logic sample;
   logic [$clog2(OVERSAMPLE)-1:0] sample_count;
-  logic mid_bit;
+  logic sample, mid_bit;
 
   // Data ---
   logic [$clog2(DATA_WIDTH)-1:0] bit_count;
 
+  rx_datapath #(.*) (
+    .data_in(rx),
+    .data_out(rx_data),
+    .*
+  );
 
 endmodule: uart_rx
-
-module rx_datapath #(
-  parameter 
-)
-();
-
-  // Counter for clk_count
-  Counter #($clog2(CLKS_PER_SAMPLE)) (
-    .D(),
-    .en(clk_en),
-    .clear(clk_clr),
-    .load(),
-    .up(1'b1),
-    .Q(clk_count)
-  );
-
-  // Counter for sample_count
-  Counter #($clog2(OVERSAMPLE)) (
-    .D(),
-    .en(sample_en),
-    .clear(sample_clr),
-    .load(),
-    .up(1'b1),
-    .Q(sample_count)
-  );
-
-  // Counter for bit_count
-  Counter #($clog2(DATA_WIDTH)) (
-    .D(),
-    .en(bit_en),
-    .clear(bit_clr),
-    .load(),
-    .up(1'b1),
-    .Q(bit_count)
-  );
-
-  // Logic for sample and mid_bit
-  assign sample = (clk_count == CLKS_PER_SAMPLE - 1);
-  assign mid_bit = (sample_count == (OVERSAMPLE/2) - 1);
-
-endmodule: rx_datapath
-
-module rx_fsm ();
-
-  enum logic [1:0] {
-    IDLE, START, DATA, STOP
-  } state, nextState;
-
-
-
-
-  always_ff @(posedge clock, posedge reset)
-endmodule: rx_fsm
